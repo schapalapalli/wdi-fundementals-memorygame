69a05dec5a90ddae8154f60d69f517b4734df9e6
Sivakumar Chapalapalli <sivakumar_chapalapalli@glic.com>
2018-09-26 17:16:21 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,29 @@
-console.log("Up and running!");
+//console.log("Up and running!");
 // Memory game cards array
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne;
-var cardTwo;
-cardOne = cards[0];
-//push card one to cards in play
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-cardTwo = cards[2];
-//push card 3 to cards in play
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
-if (cardsInPlay.length === 2){
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+
+// added for functions exercise
+var checkForMatch = function(){
+if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	}else{
 		alert("Sorry, try again.");
 	}
-	
+
 }
+var flipCard = function(cardId){
+
+var cardOne;
+var cardTwo;
+
+console.log("User Flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+
+if (cardsInPlay.length === 2){
+	checkForMatch();
+}
+
+};
+flipCard(0);
+flipCard(2);
